1c6f77bf0d2c71bfeb1dbb577cc7086742031a7e
Crazyglue <edbz2k2@gmail.com>
2017-05-29 16:33:58 -0500

    Add transmission to project section

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import port01 from '../../assets/images/grbl-parser.gif';
-import port02 from '../../assets/images/port02.jpg';
+import port02 from '../../assets/images/transmission.gif';
 import port03 from '../../assets/images/port03.jpg';
 
 class Portfolio extends Component {
@@ -10,23 +10,32 @@ class Portfolio extends Component {
       <div className="container desc">
 
         <div className="row">
-          <div className="col-md-2 col-md-offset-1">
+          <div className="col-lg-2 col-lg-offset-1">
             <h5>SIDE-PROJECTS</h5>
           </div>
-          <div className="col-md-3">
-            <p>GRBL-PARSER <a href="http://github.com/Crazyglue/grbl-parser"><i className="fa fa-github"></i> </a>
-            <a href="https://runkit.com/5812b6009d0cb70013f5b92b/59179e16a8b8390011a55e1f"><i className="fa fa-codepen"></i> </a>
 
+          <div className="col-lg-6">
+            <p>GRBL-PARSER <a href="http://github.com/Crazyglue/grbl-parser"><i className="fa fa-github"></i> </a>
+              <a href="https://runkit.com/5812b6009d0cb70013f5b92b/59179e16a8b8390011a55e1f"><i className="fa fa-codepen"></i> </a>
             </p>
-          </div>
-          <div className="col-md-6">
             <p><img className="img-responsive" src={port01} alt="" /></p>
-          </div>
-          <div className="col-md-3">
+
             <p><more>A parsing library for <a href="http://github.com/gnea/grbl">grbl</a>, written in Javascript. It takes a string, output by grbl, and parses its status, setting, state information (and more!) from it. It supports many versions of grbl, and outputs this data in a structured, predictable format.<br/><br/>
             </more>
             </p>
           </div>
+
+          <div className="col-lg-6 col-lg-offset-3">
+
+            <p>TRANSMISSION-FETCH <a href="http://github.com/Crazyglue/transmission-fetch"><i className="fa fa-github"></i> </a>
+            </p>
+            <p><img className="img-responsive" src={port02} alt="" /></p>
+
+            <p><more>
+              An API library to interface with <a href="https://transmissionbt.com/">transmission</a>.<br/><br/>
+            </more></p>
+          </div>
+
         </div>
 
         <br />
