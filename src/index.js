fff0abf7d4179d29b6aaac4fd5e1b065d0aaa78b
Crazyglue <edbz2k2@gmail.com>
2017-05-31 23:13:38 -0500

    Delete serviceworker code because i dont have an SSL cert for this site (yet?)

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import 'smoothscroll'
 import App from './containers/App';
-// Dont have an SSL cert for this website.
-// import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
